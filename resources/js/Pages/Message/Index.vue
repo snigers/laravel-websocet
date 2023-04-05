7d34450ded760c8fbec16f71a43ca1a6e95ec664
snigers <educon@yandex.ru>
2023-04-05 17:53:59 +0300

    end day 05.04.23

---
+++ resources/js/Pages/Message/Index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="w-1/2 mx-auto py-6">
+        <div v-if="messages.length > 0" class="mb-4">
+            <h3>Messages</h3>
+            <div>
+                <div v-for="message in messages">
+                    <p>{{ message.id }}</p>
+                    <p>{{ message.body }}</p>
+                    <p>{{ message.time }}</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="mb-4">
+                <input type="text" v-model="body" class="rounded-full border border-gray-400" placeholder="your message">
+            </div>
+            <div class="mb-4">
+                <a href="#" class="rounded-lg block w-48 bg-sky-400 text-white text-center py-2">Send</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index",
+
+    props: [
+        'messages'
+    ],
+
+    data() {
+        return {
+            body: '',
+        };
+    },
+
+    methods: {
+        store() {
+            axios.post('/messages', {body: this.body})
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
