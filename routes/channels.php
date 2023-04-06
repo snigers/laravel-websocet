ea09c7e7acb4b4bf43094b75538065bc3479e639
snigers <educon@yandex.ru>
2023-04-06 16:38:18 +0300

    end video fix commit

--- routes/channels.php
+++ routes/channels.php
@@ -17,6 +17,6 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('send_like_{id}', function ($user, $id) {
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
